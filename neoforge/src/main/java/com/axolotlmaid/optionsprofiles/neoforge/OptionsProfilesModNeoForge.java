d031876914514812cd7db0090b5ba529915be34f
axolotlmaid <hello@axolotlmaid.com>
2024-06-03 00:46:49 +0100

    Add NeoForge

---
+++ neoforge/src/main/java/com/axolotlmaid/optionsprofiles/neoforge/OptionsProfilesModNeoForge.java
@@ -0,0 +1,12 @@
+package com.axolotlmaid.optionsprofiles.neoforge;
+
+import net.neoforged.fml.common.Mod;
+
+import com.axolotlmaid.optionsprofiles.OptionsProfilesMod;
+
+@Mod(OptionsProfilesMod.MOD_ID)
+public final class OptionsProfilesModNeoForge {
+    public OptionsProfilesModNeoForge() {
+        OptionsProfilesMod.init();
+    }
+}
