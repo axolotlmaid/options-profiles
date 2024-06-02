2690515316b01da98b91bccbb585297ba303704b
axolotlmaid <124442837+axolotlmaid@users.noreply.github.com>
2024-06-02 17:27:15 +0100

    Add Neoforge and upgrade template
    Forge is not working but I can't really do anything about it

---
+++ neoforge/src/main/java/com/axolotlmaid/optionsprofiles/neoforge/OptionsProfilesModNeoForge.java
@@ -0,0 +1,11 @@
+package com.axolotlmaid.optionsprofiles.forge;
+
+import com.axolotlmaid.optionsprofiles.OptionsProfilesMod;
+import net.neoforged.fml.common.Mod;
+
+@Mod(OptionsProfilesMod.MOD_ID)
+public class OptionsProfilesModNeoForge {
+    public OptionsProfilesModNeoForge() {
+        OptionsProfilesMod.init();
+    }
+}
